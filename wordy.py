967f695ba91550e722073d8a9d378b5ae5b7ecda
Jacob Shulman <shulmaja@dukes.jmu.edu>
2022-02-23 23:29:10 -0500

    Fixed duplicate letter issue.

--- wordy.py
+++ wordy.py
@@ -8,18 +8,25 @@ import random
 
 def main():
     words = []
-    with open('5letterwords.txt') as f:
+    with open('5letterwords.txt') as f: # read in words
         words = f.readlines()
+    
+    validGuesses = []
+    with open('validGuesses.txt') as j: # read in valid guesses
+        validGuesses = j.readlines()
+    for i in range(len(validGuesses)): # remove new line from each word
+        validGuesses[i] = validGuesses[i].strip()
     
     play = "y"
     while play == "y" or play == "yes":
         word = startGame(words)
-        playGame(word)
+        playGame(word, validGuesses)
         print("Play again? y/n: ", end = "")
         play = str(input())
 
     print("Goodbye")
 
+# Gets the random word
 def startGame(words):
     print("Welcome to Wordy. You have 6 tries to guess a random 5 letter word.")
     randomNumber = random.randrange(0, 5786)
@@ -27,22 +34,43 @@ def startGame(words):
     return word
 
 
-def playGame(word):
-    # 🟨    🟩   ⬛️
+def playGame(word, vG):
+
     for i in range(6):
         guess = str(input())
 
-        while len(guess) != 5:
-            print("Please enter a 5 letter word:")
+        while len(guess) != 5 or guess not in vG:
+            print("Please enter a valid 5 letter word:")
             guess = str(input())
 
-        for j in range(5):
-            if guess[j] == word[j]:
+
+        # dictionary to store colors of each letter, start as all black
+        results = {i: "black" for i in range(5)}
+
+        # counts the number of times each letter is in the guess
+        count = {i: 0 for i in set(guess)} # convert to set to eliminate duplicates
+
+        g = 0
+        for i in guess: # check for greens
+            count[i] += 1
+            if i == word[g]:
+                results[g] = "green"
+            g += 1
+        
+        y = 0
+        for i in guess: # check for yellows
+            if i in word and word.count(i) >= count[i] and results[y] != "green":
+                results[y] = "yellow"
+            y += 1
+
+        for i in range(5): # print the colors
+            if results[i] == "green":
                 print("🟩", end= "")
-            elif guess[j] in word:
+            elif results[i] == "yellow":
                 print("🟨", end = "")
             else:
                 print("⬛️", end = "")
+        
         print()
         if guess == str(word):
             print("Good job! The word was", word)
@@ -54,3 +82,32 @@ def playGame(word):
 
 if __name__ == "__main__":
     main()
+
+
+        # colors = ["black"] * 5
+
+        # for j in range(5): # need to break this up into 3 loops
+        #     if guess[j] == word[j]:
+        #         print("🟩", end= "")
+        #     elif guess[j] in word:
+        #         print("🟨", end = "")
+        #     else:
+        #         print("⬛️", end = "")
+        
+        # need to do this in three loops to fix the letter issue
+        # for j in range(5):
+        #     if guess[j] == word[j]:
+        #         colors[j] = "green"
+        # for j in range(4, -1, -1):
+        #     if guess[j] in word and colors[j] != "green":
+        #         colors[j] = "yellow"
+        
+        # # .find() method
+    
+        # for j in range(5):
+        #     if colors[j] == "green":
+        #         print("🟩", end= "")
+        #     elif colors[j] == "yellow":
+        #         print("🟨", end = "")
+        #     else:
+        #         print("⬛️", end = "")
